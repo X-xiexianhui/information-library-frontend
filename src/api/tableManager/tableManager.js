0b0b2dce9986348a93a47b91a5085485fe6b8de0
xiexianhui <xiexianhuicn@outlook.com>
2022-04-01 21:42:27 +0800

    "tableManager"

--- src/api/tableManager/tableManager.js
+++ src/api/tableManager/tableManager.js
@@ -16,6 +16,7 @@ export function insertEvent (ref, record) {
 // 移除数据
 export async function removeEvent (ref) {
   const selectRecords = ref.getCurrentRecord()
+  // console.log(selectRecords)
   if (selectRecords.length) {
     const type = await VXETable.modal.confirm('您确定要删除选中的数据吗?')
     if (type === 'confirm') {
