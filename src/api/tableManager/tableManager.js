d7d443a19430149503cb8c341f12bc04228fd253
xiexianhui <xiexianhuicn@outlook.com>
2022-04-02 16:09:55 +0800

    "tableManager"

--- src/api/tableManager/tableManager.js
+++ src/api/tableManager/tableManager.js
@@ -56,16 +56,25 @@ async function fullValidEvent (ref) {
   }
   return errMap
 }
-// 创建新表
-export async function createTable (ref, tableForm) {
-  if (tableForm.db_name === '' || tableForm.tb_name === '') {
-    return error('请输入数据库和表名')
-  }
+function beforeSave (ref) {
   const { insertRecords, removeRecords, updateRecords } = ref.getRecordset()
   const Saved = insertRecords.length === 0 && removeRecords.length === 0 && updateRecords.length === 0
   if (Saved) {
     return error('请输入数据')
   }
+  return { insertRecords, removeRecords, updateRecords }
+}
+// 创建新表
+export async function createTable (ref, tableForm) {
+  if (tableForm.db_name === '' || tableForm.tb_name === '') {
+    return error('请输入数据库和表名')
+  }
+  // const { insertRecords, removeRecords, updateRecords } = ref.getRecordset()
+  // const Saved = insertRecords.length === 0 && removeRecords.length === 0 && updateRecords.length === 0
+  // if (Saved) {
+  //   return error('请输入数据')
+  // }
+  const insertRecords = beforeSave(ref)
   if (await fullValidEvent(ref)) {
     return
   }
@@ -89,15 +98,33 @@ export async function createTable (ref, tableForm) {
 }
 // 修改表
 export async function editTable (ref, tableForm) {
-  const { insertRecords, removeRecords, updateRecords } = ref.getRecordset()
-  const Saved = insertRecords.length === 0 && removeRecords.length === 0 && updateRecords.length === 0
-  if (Saved) {
-    return error('请输入数据')
-  }
+  // const { insertRecords, removeRecords, updateRecords } = ref.getRecordset()
+  // const Saved = insertRecords.length === 0 && removeRecords.length === 0 && updateRecords.length === 0
+  // if (Saved) {
+  //   return error('请输入数据')
+  // }
+  const { insertRecords, removeRecords, updateRecords } = beforeSave(ref)
   if (await fullValidEvent(ref)) {
     return
   }
-  ref.reloadData(this.tableData)
+  let data = {
+    db_name: tableForm.db_name,
+    tb_name: tableForm.tb_name,
+    insert: insertRecords,
+    remove: removeRecords,
+    update: updateRecords
+  }
+  try {
+    const res = await axios.post('/api/tb/alter', data)
+    if (res.data.code !== 200) {
+      error(res.data.msg)
+    } else {
+      Message.success(res.data.msg)
+      ref.reloadData(res.data.data)
+    }
+  } catch (e) {
+    error(e)
+  }
 }
 export function checkEdit (ref) {
   const updateRecords = ref.getRecordset()
