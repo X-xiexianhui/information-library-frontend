4ba1079bb14c670413e8c49a16bf2b4e02c2d65b
xiexianhui <xiexianhuicn@outlook.com>
2022-04-18 11:05:21 +0800

    "menu manager"

---
+++ src/components/menuManager/page/menuPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <page-head class="nav"></page-head>
+    <el-container>
+      <el-card>
+        <menu-list></menu-list>
+      </el-card>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import PageHead from '../../common/head/pageHead'
+import MenuList from '../components/menuList'
+export default {
+  name: 'menuPage',
+  components: {MenuList, PageHead}
+}
+</script>
+
+<style scoped>
+.el-card{
+  width: 60%;
+  margin-left: 20%;
+}
+</style>
