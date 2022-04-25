38dad9227fb3c447ed5fce54701770bb9bdd8c1b
xiexianhui <xiexianhuicn@outlook.com>
2022-04-25 16:40:03 +0800

    "fix"

--- src/components/common/head/Menu.vue
+++ src/components/common/head/Menu.vue
@@ -19,6 +19,26 @@
           <i class="el-icon-s-data" :class="$style.menuIcon"></i>表单管理
         </a>
       </el-menu-item>
+      <el-menu-item index="4">
+        <a :class="$style.menuLink" href="/menu" target="_blank">
+          <i class="el-icon-s-data" :class="$style.menuIcon"></i>菜单管理
+        </a>
+      </el-menu-item>
+      <el-menu-item index="5">
+        <a :class="$style.menuLink" href="/role" target="_blank">
+          <i class="el-icon-s-data" :class="$style.menuIcon"></i>角色管理
+        </a>
+      </el-menu-item>
+      <el-menu-item index="6">
+        <a :class="$style.menuLink" href="/user" target="_blank">
+          <i class="el-icon-s-data" :class="$style.menuIcon"></i>用户管理
+        </a>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <a :class="$style.menuLink" href="/auth" target="_blank">
+          <i class="el-icon-s-data" :class="$style.menuIcon"></i>权限管理
+        </a>
+      </el-menu-item>
     </el-menu>
 </template>
 
