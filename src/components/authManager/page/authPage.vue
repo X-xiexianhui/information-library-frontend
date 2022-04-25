0bcba614cd339d42902aa3924471c80e660b6ae5
xiexianhui <xiexianhuicn@outlook.com>
2022-04-25 11:23:36 +0800

    "auth manager"

---
+++ src/components/authManager/page/authPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+   <page-head class="nav"></page-head>
+    <el-container>
+      <el-card>
+       <auth-list></auth-list>
+      </el-card>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import AuthList from '../components/authList'
+import PageHead from '../../common/head/pageHead'
+export default {
+  name: 'authPage',
+  components: {PageHead, AuthList}
+}
+</script>
+
+<style scoped>
+.el-card{
+  width: 60%;
+  margin-left: 20%;
+}
+</style>
