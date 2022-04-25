0edd878344083754c41dd9957b1c8a595ef90cd3
xiexianhui <xiexianhuicn@outlook.com>
2022-04-25 21:37:02 +0800

    "fix"

--- src/components/roleManager/components/roleEditForm.vue
+++ src/components/roleManager/components/roleEditForm.vue
@@ -45,7 +45,6 @@ export default {
     bus.$on('showEditForm', () => {
       this.dialogVisible = true
     })
-    console.log(this.role_data)
   },
   data () {
     return {
@@ -54,6 +53,11 @@ export default {
       formRule: {
         role_name: [{required: true, message: '请输入角色名称', trigger: 'blur'}]
       }
+    }
+  },
+  watch: {
+    role_data (newValue) {
+      this.form = JSON.parse(JSON.stringify(newValue))
     }
   },
   methods: {
