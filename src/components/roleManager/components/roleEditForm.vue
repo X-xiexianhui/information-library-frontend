93695f1fd8c2da28d21c539b998453b39da56b2c
xiexianhui <xiexianhuicn@outlook.com>
2022-04-25 09:31:19 +0800

    "role manager"

---
+++ src/components/roleManager/components/roleEditForm.vue
@@ -0,0 +1,89 @@
+<template>
+  <el-dialog
+    :visible.sync="dialogVisible"
+    :before-close="closeEvent"
+  >
+    <el-form ref="form" :model="form" :rules="formRule" label-width="80px">
+      <el-form-item label="角色名称" prop="menu_name">
+        <el-input v-model="form.role_name" placeholder="请输入角色名称"></el-input>
+      </el-form-item>
+      <el-form-item label="菜单级别" prop="menu_level">
+        <el-input
+          type="textarea"
+          :rows="2"
+          placeholder="请输入角色描述"
+          v-model="form.role_description">
+        </el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+        <el-button @click="closeEvent">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </span>
+  </el-dialog>
+</template>
+
+<script>
+import {error} from '../../../api/error'
+import axios from 'axios'
+
+export default {
+  name: 'roleEditForm',
+  props: {
+    Visible: {
+      type: Boolean,
+      default: false
+    },
+    role_id: {
+      type: Number,
+      default: -1
+    }
+  },
+  created () {
+  },
+  data () {
+    return {
+      form: {
+        role_id: this.role_id,
+        role_name: '',
+        role_description: ''
+      },
+      dialogVisible: this.Visible.valueOf(),
+      formRule: {
+        role_name: [{required: true, message: '请输入角色名称'}]
+      }
+    }
+  },
+  methods: {
+    closeEvent () {
+      this.dialogVisible = false
+      this.$refs.form.resetFields()
+    },
+    async save () {
+      await this.$refs.form.validate(async valid => {
+        if (!valid) return
+        try {
+          let res
+          if (this.role_id === -1) {
+            res = await axios.post('/api/role/add', this.form)
+          } else {
+            res = await axios.post('/api/role/edit', this.form)
+          }
+          if (res.data.code !== 200) {
+            error(res.data.msg)
+          } else {
+            this.tableData = res.data.data.reverse()
+            this.closeEvent()
+          }
+        } catch (e) {
+          error(e.message)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/roleManager/components/roleList.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <vxe-grid
+      border
+      resizable
+      ref="menuTable"
+      :toolbar-config="toolBarConfig"
+      :row-config="{isCurrent: true}"
+      :columns="tableColumn"
+      :data="tableData">
+      <template #toolbar_buttons>
+        <vxe-input v-model="searchName" placeholder="请输入角色名称" clearable></vxe-input>
+        <vxe-button status="primary" @click="query(searchName)">搜索</vxe-button>
+        <vxe-button status="success" @click="dialogVisible = true">新增</vxe-button>
+        <vxe-button status="success" @click="editEvent">修改</vxe-button>
+        <vxe-button status="success" @click="removeEvent">删除</vxe-button>
+        <vxe-button @click="$refs.menuTable.exportData()">导出</vxe-button>
+      </template>
+      <template #pager>
+        <vxe-pager
+          :layouts="['Sizes', 'PrevJump', 'PrevPage', 'Number', 'NextPage', 'NextJump', 'FullJump', 'Total']"
+          :current-page.sync="tablePage.currentPage"
+          :page-size.sync="tablePage.pageSize"
+          :total="tableData.length"
+          @page-change="handlePageChange">
+        </vxe-pager>
+      </template>
+    </vxe-grid>
+  </div>
+</template>
+
+<script>
+import {error} from '../../../api/error'
+import axios from 'axios'
+
+export default {
+  name: 'roleList',
+  data () {
+    return {
+      toolBarConfig: {
+        slots: {
+          buttons: 'toolbar_buttons'
+        }
+      },
+      form_name: '',
+      dialogVisible: false,
+      form_id: -1,
+      searchName: '',
+      tablePage: {
+        currentPage: 1,
+        pageSize: 10
+      },
+      tableColumn: [
+        {field: 'menu_name', title: '菜单名称'},
+        {field: 'menu_level', title: '菜单级别'},
+        {field: 'father_menu', title: '父菜单'},
+        {field: 'context_form', title: '关联表单'}
+      ],
+      tableData: []
+    }
+  },
+  created () {
+    this.query('')
+  },
+  methods: {
+    async query (val) {
+      try {
+        const res = await this.$http.get('/api/role/query', {params: {menu_name: val}})
+        if (res.data.code !== 200) {
+          error(res.data.msg)
+        } else {
+          this.tableData = res.data.data.reverse()
+        }
+      } catch (e) {
+        error(e)
+      }
+    },
+    handlePageChange ({currentPage, pageSize}) {
+      this.tablePage.currentPage = currentPage
+      this.tablePage.pageSize = pageSize
+    },
+    removeEvent () {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        try {
+          const selectRecords = this.$refs.menuTable.getCurrentRecord()
+          const res = await axios.post('/api/role/delete', {
+            menu_id: selectRecords.menu_id
+          })
+          if (res.data.code !== 200) {
+            error(res.data.msg)
+          } else {
+            this.tableData = res.data.data.reverse()
+          }
+        } catch (e) {
+          error(e.message)
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    editEvent () {
+      const selectRecords = this.$refs.menuTable.getCurrentRecord()
+      if (!selectRecords) {
+        return error('请先选择需要修改的数据')
+      }
+      this.form_id = selectRecords.form_id
+      this.dialogVisible = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
