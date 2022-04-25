15d615c5329a0ab757830ac39e12280093fb79e9
xiexianhui <xiexianhuicn@outlook.com>
2022-04-25 23:25:22 +0800

    "fix"

--- src/components/dataManager/page/home.vue
+++ src/components/dataManager/page/home.vue
@@ -1,11 +1,20 @@
 <template>
-<el-container>
-  <el-aside>
-  </el-aside>
-  <el-main>
-    <router-view/>
-  </el-main>
-</el-container>
+  <div>
+    <div :class="$style.box">
+      <div></div>
+      <div :class="$style.btns">
+        <slot></slot>
+      </div>
+    </div>
+    <el-container>
+      <el-aside>
+      </el-aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </div>
+
 </template>
 
 <script>
@@ -13,8 +22,19 @@ export default {
   name: 'home'
 }
 </script>
-<style scoped>
-/deep/.el-container{
-  margin-top: 0px;
+<style module>
+.box {
+  top: 0;
+  position: fixed;
+  margin: 0 0;
+  padding: 0 0;
+  height: auto;
+  width: 100%;
+  background: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 8px 1px rgba(0,0,0,.3);
+  z-index: 3;
 }
 </style>
