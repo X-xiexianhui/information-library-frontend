3ac3ccd6f4743bb6801681e072c387bf3d5f853f
xiexianhui <xiexianhuicn@outlook.com>
2022-04-25 16:00:45 +0800

    "user manager"

---
+++ src/components/userManager/components/userAddForm.vue
@@ -0,0 +1,99 @@
+<template>
+  <el-dialog
+    :visible.sync="dialogVisible"
+    :before-close="closeEvent"
+  >
+    <el-form ref="form" :model="form" :rules="formRule" label-width="80px">
+      <el-form-item label="角色名称" prop="role_name">
+        <el-input v-model="form.role_name" placeholder="请输入角色名称"></el-input>
+      </el-form-item>
+      <el-form-item label="角色描述" prop="role_description">
+        <el-input
+          type="textarea"
+          :rows="2"
+          placeholder="请输入角色描述"
+          v-model="form.role_description">
+        </el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+        <el-button @click="closeEvent">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </span>
+  </el-dialog>
+</template>
+
+<script>
+import {error} from '../../../api/error'
+import axios from 'axios'
+
+export default {
+  name: 'userAddForm',
+  props: {
+    Visible: {
+      type: Boolean,
+      default: false
+    },
+    role_id: {
+      type: Number,
+      default: -1
+    }
+  },
+  created () {
+    this.queryById()
+  },
+  data () {
+    return {
+      form: {
+        role_id: this.role_id,
+        role_name: '',
+        role_description: ''
+      },
+      dialogVisible: this.Visible.valueOf(),
+      formRule: {
+        role_name: [{required: true, message: '请输入角色名称', trigger: 'blur'}]
+      }
+    }
+  },
+  methods: {
+    closeEvent () {
+      this.dialogVisible = false
+      this.$refs.form.resetFields()
+    },
+    async queryById () {
+      if (this.role_id !== -1) {
+        try {
+          const res = await this.$http.get('/api/role/id', {params: {role_id: this.role_id}})
+          if (res.data.code !== 200) {
+            error(res.data.msg)
+          } else {
+            this.form = res.data.data
+          }
+        } catch (e) {
+          error(e)
+        }
+      }
+    },
+    async save () {
+      await this.$refs.form.validate(async valid => {
+        if (!valid) return
+        try {
+          let res = await axios.post('/api/user/add', this.form)
+          if (res.data.code !== 200) {
+            error(res.data.msg)
+          } else {
+            this.tableData = res.data.data.reverse()
+            this.closeEvent()
+          }
+        } catch (e) {
+          error(e.message)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/userManager/components/userList.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <vxe-grid
+      border
+      resizable
+      ref="userTable"
+      :toolbar-config="toolBarConfig"
+      :row-config="{isCurrent: true}"
+      :columns="tableColumn"
+      :data="tableData">
+      <template #toolbar_buttons>
+        <vxe-input v-model="searchName" placeholder="请输入角色名称" clearable></vxe-input>
+        <vxe-button status="primary" @click="query(searchName)">搜索</vxe-button>
+        <vxe-button status="success" @click="dialogVisible = true">新增</vxe-button>
+        <vxe-button status="success" @click="editEvent">修改</vxe-button>
+        <vxe-button status="success" @click="removeEvent">删除</vxe-button>
+        <vxe-button @click="$refs.userTable.exportData()">导出</vxe-button>
+      </template>
+      <template #pager>
+        <vxe-pager
+          :layouts="['Sizes', 'PrevJump', 'PrevPage', 'Number', 'NextPage', 'NextJump', 'FullJump', 'Total']"
+          :current-page.sync="tablePage.currentPage"
+          :page-size.sync="tablePage.pageSize"
+          :total="tableData.length"
+          @page-change="handlePageChange">
+        </vxe-pager>
+      </template>
+    </vxe-grid>
+  </div>
+</template>
+
+<script>
+import {error} from '../../../api/error'
+import axios from 'axios'
+
+export default {
+  name: 'userList',
+  data () {
+    return {
+      toolBarConfig: {
+        slots: {
+          buttons: 'toolbar_buttons'
+        }
+      },
+      dialogVisible: false,
+      role_id: -1,
+      searchName: '',
+      tablePage: {
+        currentPage: 1,
+        pageSize: 10
+      },
+      tableColumn: [
+        {field: 'user_id', title: '用户账号'},
+        {field: 'user_name', title: '用户名称'},
+        {field: 'user_email', title: '用户邮箱'},
+        {field: 'user_role', title: '用户角色'},
+        {field: 'is_use', title: '是否禁用'}
+      ],
+      tableData: []
+    }
+  },
+  created () {
+    this.query('')
+  },
+  methods: {
+    async query (val) {
+      try {
+        const res = await this.$http.get('/api/user/query', {params: {user_name: val}})
+        if (res.data.code !== 200) {
+          error(res.data.msg)
+        } else {
+          this.tableData = res.data.data.reverse()
+          this.page()
+        }
+      } catch (e) {
+        error(e)
+      }
+    },
+    handlePageChange ({currentPage, pageSize}) {
+      this.tablePage.currentPage = currentPage
+      this.tablePage.pageSize = pageSize
+      this.currentData = this.tableData.slice((currentPage - 1) * pageSize, pageSize * currentPage)
+    },
+    removeEvent () {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        try {
+          const selectRecords = this.$refs.userTable.getCurrentRecord()
+          const res = await axios.post('/api/user/delete', {
+            user_id: selectRecords.user_id
+          })
+          if (res.data.code !== 200) {
+            error(res.data.msg)
+          } else {
+            this.tableData = res.data.data.reverse()
+            this.page()
+          }
+        } catch (e) {
+          error(e.message)
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    editEvent () {
+      const selectRecords = this.$refs.userTable.getCurrentRecord()
+      if (!selectRecords) {
+        return error('请先选择需要修改的数据')
+      }
+      this.role_id = selectRecords.role_id
+      this.dialogVisible = true
+    },
+    page () {
+      const currentPage = this.tablePage.currentPage
+      const pageSize = this.tablePage.pageSize
+      this.currentData = this.tableData.slice((currentPage - 1) * pageSize, pageSize * currentPage)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
