c54f96edc3b1a29d9fa08a4f102f911aff033194
xiexianhui <xiexianhuicn@outlook.com>
2022-04-26 15:28:32 +0800

    "fix"

--- src/components/userManager/components/userEditForm.vue
+++ src/components/userManager/components/userEditForm.vue
@@ -56,6 +56,7 @@ export default {
       form: JSON.parse(JSON.stringify(this.user_data)),
       dialogVisible: false,
       formRule: {
+        user_name: [ {required: true, message: '请输入用户名称', trigger: 'blur'}],
         user_email: [
           {required: true, message: '请输入用户邮箱', trigger: 'blur'},
           {pattern: /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/, message: '邮箱格式错误', trigger: 'blur'}
