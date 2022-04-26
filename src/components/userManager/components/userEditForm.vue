e8572638e864c9a57248ff37c1127bdfee2d2292
xiexianhui <xiexianhuicn@outlook.com>
2022-04-26 15:18:38 +0800

    "fix"

--- src/components/userManager/components/userEditForm.vue
+++ src/components/userManager/components/userEditForm.vue
@@ -5,7 +5,7 @@
   >
     <el-form ref="form" :model="form" :rules="formRule" label-width="80px">
       <el-form-item label="用户账号" prop="user_id">
-        <el-input v-model="form.user_id" placeholder="请输入用户账号"></el-input>
+        <span>{{form.user_id}}</span>
       </el-form-item>
       <el-form-item label="用户名称" prop="user_name">
         <el-input placeholder="请输入角色描述" v-model="form.user_name">
@@ -24,15 +24,8 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="是否禁用" prop="disabled">
-        <el-switch
-          v-model="form.disabled"
-          active-text="禁用"
-          inactive-text="启用">
-        </el-switch>
-      </el-form-item>
     </el-form>
-    <span slot="footer" class="dialog-footer">
+    <span style="margin: auto">
         <el-button @click="closeEvent">取 消</el-button>
         <el-button type="primary" @click="save">确 定</el-button>
       </span>
@@ -43,6 +36,7 @@
 import {error} from '../../../api/error'
 import axios from 'axios'
 import bus from '../../../common/bus'
+import {getUpdate} from '../../../common/getUpdate'
 
 export default {
   name: 'userEditForm',
@@ -62,7 +56,6 @@ export default {
       form: JSON.parse(JSON.stringify(this.user_data)),
       dialogVisible: false,
       formRule: {
-        user_id: [{required: true, message: '请输入用户账号', trigger: 'blur', length: 4}],
         user_email: [
           {required: true, message: '请输入用户邮箱', trigger: 'blur'},
           {pattern: /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/, message: '邮箱格式错误', trigger: 'blur'}
@@ -104,7 +97,10 @@ export default {
       await this.$refs.form.validate(async valid => {
         if (!valid) return
         try {
-          let res = await axios.post('/api/user/edit', this.form)
+          let res = await axios.post('/api/user/edit', {
+            user_id: this.form.user_id,
+            update: getUpdate(this.user_data, this.form)
+          })
           if (res.data.code !== 200) {
             error(res.data.msg)
           } else {
