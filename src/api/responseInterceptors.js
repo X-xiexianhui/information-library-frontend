1b2e203d38b4a93fc0cb0257e1deef944c11b327
xiexianhui <xiexianhuicn@outlook.com>
2022-04-28 20:33:52 +0800

    "fix"

--- src/api/responseInterceptors.js
+++ src/api/responseInterceptors.js
@@ -4,9 +4,9 @@ import router from '../router'
 axios.interceptors.response.use(response => {
   const res = response.data || {}
 
-  if (res.code === 200) {
+  if (res.data.code === 200) {
     return Promise.resolve(res)
-  } else if (res.code === 401 || res.code === 403) {
+  } else if (res.data.code === 401 || res.data.code === 403) {
     return router.replace({
       path: '/'
     })
