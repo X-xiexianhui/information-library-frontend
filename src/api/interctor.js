606844890fe2a5753845d77ea9a9b82e07c97aae
xiexianhui <xiexianhuicn@outlook.com>
2022-04-28 22:53:42 +0800

    "data manage"

---
+++ src/api/interctor.js
@@ -0,0 +1,14 @@
+import router from '../router'
+import {MessageBox} from 'element-ui'
+
+export function interceptor (res) {
+  if (res.code === 401 || res.code === 403) {
+    return router.push('/')
+  } else {
+    MessageBox.alert(res.message, '警告', {
+      confirmButtonText: '确定',
+      callback: () => {
+      }
+    }).then(() => {})
+  }
+}
--- src/components/dataManager/components/dataMenu.vue
+++ src/components/dataManager/components/dataMenu.vue
@@ -1,13 +1,50 @@
 <template>
-
+<el-menu
+  text-color="#000000"
+  active-text-color="#ffd04b"
+>
+  <el-submenu v-for="(index,parent) in menuList" :index="index" :key="index">
+    <template slot="title">
+      <span>{{parent.name}}</span>
+    </template>
+    <el-menu-item v-for="child in parent.child" :key="child.menu_id">
+      <a :class="$style.menuLink" :href="'/#/home/data?form_id='+child.form_id" target="_blank">
+        数据库管理
+      </a>
+    </el-menu-item>
+  </el-submenu>
+</el-menu>
 </template>
 
 <script>
 export default {
-  name: 'dataMenu'
+  name: 'dataMenu',
+  created () {
+    this.form_id = this.$route.params.form_id
+    console.log(this.form_id)
+  },
+  data () {
+    return {
+      menuList: [],
+      form_id: 0
+    }
+  },
+  methods: {
+    async initMenu () {
+      try {
+
+      } catch (e) {
+
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style module>
+.menuLink {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
 </style>
