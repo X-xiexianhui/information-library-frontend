f76215c369d0120c443f38a7ee0fe118eb01acc9
xiexianhui <xiexianhuicn@outlook.com>
2022-04-28 20:41:27 +0800

    "fix"

--- src/api/error.js
+++ src/api/error.js
@@ -1,5 +1,14 @@
 import {MessageBox} from 'element-ui'
+import router from '../router'
 
-export function error (message) {
-  MessageBox.alert(message, '警告', {confirmButtonText: '确定', callback: () => {}})
+export function error (res) {
+  if (res.code === 401 || res.code === 403) {
+    return router.push('/')
+  } else {
+    MessageBox.alert(res.message, '警告', {
+      confirmButtonText: '确定',
+      callback: () => {
+      }
+    }).then(() => {})
+  }
 }
