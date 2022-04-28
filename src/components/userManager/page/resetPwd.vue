7cbc8b651ff28210f7ea2937afe352ce1512f7ff
xiexianhui <xiexianhuicn@outlook.com>
2022-04-28 14:51:12 +0800

    "fix"

---
+++ src/components/userManager/page/resetPwd.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <vue-particles
+    class="login-background"
+    color="#97D0F2"
+    :particleOpacity="0.7"
+    :particlesNumber="50"
+    shapeType="circle"
+    :particleSize="4"
+    linesColor="#97D0F2"
+    :linesWidth="1"
+    :lineLinked="true"
+    :lineOpacity="0.4"
+    :linesDistance="150"
+    :moveSpeed="3"
+    :hoverEffect="true"
+    hoverMode="grab"
+    :clickEffect="true"
+    clickMode="push">
+  </vue-particles>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'resetPwd'
+}
+</script>
+
+<style scoped>
+.login-background {
+  background: linear-gradient(0, #dcf2e6 0%, #ffffff 100%);
+  width: 100%;
+  height: 100%; /**宽高100%是为了图片铺满屏幕 */
+  z-index: -1;
+  position: absolute;
+}
+</style>
