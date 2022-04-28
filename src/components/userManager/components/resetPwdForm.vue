b444dd72a63c8f3f4f9675ff7c5e46cafa6a48fa
xiexianhui <xiexianhuicn@outlook.com>
2022-04-28 16:26:42 +0800

    "fix"

--- src/components/userManager/components/resetPwdForm.vue
+++ src/components/userManager/components/resetPwdForm.vue
@@ -1,61 +1,64 @@
 <template>
   <div class="page-container">
-  <div class="forgot-password">
-    <el-button type="text" v-show="active>= 1" class="return-page-wr" @click="active--"><i
-      class="el-icon-arrow-left"></i> 上一步
-    </el-button>
-    <el-button type="text" v-show="active=== 0" class="return-page-wr" @click="$router.push({path:'/'})"><i
-      class="el-icon-arrow-left"></i> 返回
-    </el-button>
-    <div class="content">
-      <el-steps :active="active" finish-status="success">
-        <el-step title="账号验证"></el-step>
-        <el-step title="重置密码"></el-step>
-        <el-step title="完成设置"></el-step>
-      </el-steps>
-      <div v-if="active === 0">
-        <el-form :model="emailForm">
-          <el-form-item>
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item>
+    <div class="forgot-password">
+      <el-button type="text" v-show="active>= 1" class="return-page-wr" @click="active--"><i
+        class="el-icon-arrow-left"></i> 上一步
+      </el-button>
+      <el-button type="text" v-show="active=== 0" class="return-page-wr" @click="$router.push({path:'/'})"><i
+        class="el-icon-arrow-left"></i> 返回
+      </el-button>
+      <div class="content">
+        <el-steps :active="active" finish-status="success">
+          <el-step title="账号验证"></el-step>
+          <el-step title="重置密码"></el-step>
+          <el-step title="完成设置"></el-step>
+        </el-steps>
+        <div v-if="active === 0">
+          <el-form :model="emailForm">
+            <el-form-item>
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item>
               <el-input>
                 <el-button plain :disabled="flag" @click="getAuthCode">{{ msg }}</el-button>
               </el-input>
-          </el-form-item>
-        </el-form>
-        <div>
-          <button type="button" class="login-btn" @click="checkEmail">下一步 <i class="el-icon-arrow-right"></i></button>
+            </el-form-item>
+          </el-form>
+          <div>
+            <button type="button" class="login-btn" @click="checkEmail">下一步 <i class="el-icon-arrow-right"></i></button>
+          </div>
         </div>
-      </div>
-      <div v-if="active=== 1">
-        <el-form ref="forgotPasswordForm" :model="forgotPasswordForm" :rules="forgotPasswordFormRules">
-          <el-form-item prop="pwd" label ="密码:">
-            <el-input v-model="forgotPasswordForm.user_pwd" type="password" placeholder="6-16位大小写字母+数字组合" auto-complete="off" >
-            </el-input>
-          </el-form-item>
-          <el-form-item prop="confirmPwd" label ="确认密码:">
-            <el-input v-model="forgotPasswordForm.confirm_pwd" placeholder="重复密码" type="password">
-            </el-input>
-          </el-form-item>
-        </el-form>
-        <div class="btn-wr">
-          <button type="button" class="login-btn" @click.prevent="resetPassword">确认 <i class="el-icon-arrow-right" ></i></button>
+        <div v-if="active=== 1">
+          <el-form ref="forgotPasswordForm" :model="forgotPasswordForm" :rules="forgotPasswordFormRules">
+            <el-form-item prop="pwd" label="密码:">
+              <el-input v-model="forgotPasswordForm.user_pwd" type="password" placeholder="6-16位大小写字母+数字组合"
+                        auto-complete="off">
+              </el-input>
+            </el-form-item>
+            <el-form-item prop="confirmPwd" label="确认密码:">
+              <el-input v-model="forgotPasswordForm.confirm_pwd" placeholder="重复密码" type="password">
+              </el-input>
+            </el-form-item>
+          </el-form>
+          <div class="btn-wr">
+            <button type="button" class="login-btn" @click.prevent="resetPassword">确认 <i
+              class="el-icon-arrow-right"></i></button>
+          </div>
         </div>
-      </div>
-      <div v-if="active=== 2">
-        <div class="reset-success">
-          <el-result icon="success" title="修改密码成功" subTitle="请重新登录">
-            <template slot="extra">
-              <div class="btn-wr">
-                <button type="button" class="login-btn" @click="$router.push({path:'/'})">登录 <i class="el-icon-arrow-right" ></i></button>
-              </div>
-            </template>
-          </el-result>
+        <div v-if="active=== 2">
+          <div class="reset-success">
+            <el-result icon="success" title="修改密码成功" subTitle="请重新登录">
+              <template slot="extra">
+                <div class="btn-wr">
+                  <button type="button" class="login-btn" @click="$router.push({path:'/'})">登录 <i
+                    class="el-icon-arrow-right"></i></button>
+                </div>
+              </template>
+            </el-result>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -85,15 +88,23 @@ export default {
       },
       forgotPasswordFormRules: {
         pwd: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 6, max: 16, message: '长度在6-16位之间', trigger: 'blur' },
-          { pattern: new RegExp('^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,16}$'), message: '密码为6-16位大小写字母+数字组合，请重新输入', trigger: 'blur' }
+          {required: true, message: '请输入密码', trigger: 'blur'},
+          {min: 6, max: 16, message: '长度在6-16位之间', trigger: 'blur'},
+          {
+            pattern: new RegExp('^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,16}$'),
+            message: '密码为6-16位大小写字母+数字组合，请重新输入',
+            trigger: 'blur'
+          }
         ],
         confirmPwd: [
-          { required: true, message: '请输入确认密码', trigger: 'blur' },
-          { validator: validateConfirmPass, trigger: 'blur' },
-          { min: 6, max: 16, message: '长度在6-16位之间', trigger: 'blur' },
-          { pattern: new RegExp('^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,16}$'), message: '密码为6-16位大小写字母+数字组合，请重新输入', trigger: 'blur' }
+          {required: true, message: '请输入确认密码', trigger: 'blur'},
+          {validator: validateConfirmPass, trigger: 'blur'},
+          {min: 6, max: 16, message: '长度在6-16位之间', trigger: 'blur'},
+          {
+            pattern: new RegExp('^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6,16}$'),
+            message: '密码为6-16位大小写字母+数字组合，请重新输入',
+            trigger: 'blur'
+          }
         ]
       }
     }
@@ -125,115 +136,116 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-.forgot-password {
-  .return-page-wr {
-    position: relative;
-    top: 30px;
-    text-align: left;
-    justify-content:left;
-    color: #67C23A;
-    font-size: 16px;
-    margin-bottom: 15px;
-    i {
-      font-size: 20px;
-      vertical-align: middle;
+  .forgot-password {
+    width: 580px;
+    height: 540px;
+    background: #FFFFFF;
+    border-radius: 6px;
+    .return-page-wr {
+      position: relative;
+      top: 30px;
+      left: 0;
+      justify-content: left;
+      color: #67C23A;
+      font-size: 16px;
+      margin-bottom: 15px;
+
+      i {
+        font-size: 20px;
+        vertical-align: middle;
+      }
     }
-  }
-  width:580px;
-  height:540px;
-  background: #FFFFFF;
-  border-radius: 6px;
-  .content {
-    padding: 30px 90px;
-    box-sizing: border-box;
-  }
-
-  /deep/ .el-step.is-horizontal {
-
-    .el-step__line {
-      height: 2px;
-      top: 20px;
-      left: 48px;
-      right: 10px;
+    .content {
+      padding: 30px 90px;
+      box-sizing: border-box;
     }
 
-  }
+    /deep/ .el-step.is-horizontal {
 
-  /deep/ .el-step__icon {
-    width: 42px;
-    height: 42px;
-    color: #67C23A;
-    font-size: 18px;
-    border: 2px solid #67C23A;
-  }
+      .el-step__line {
+        height: 2px;
+        top: 20px;
+        left: 48px;
+        right: 10px;
+      }
 
-  /deep/ .el-step__line {
-    background: #67C23A;
-  }
+    }
 
-  /deep/ .is-process {
+    /deep/ .el-step__icon {
+      width: 42px;
+      height: 42px;
+      color: #67C23A;
+      font-size: 18px;
+      border: 2px solid #67C23A;
+    }
 
-    .el-step__icon {
+    /deep/ .el-step__line {
       background: #67C23A;
-      color: #FFFFFF;
-      border: 0;
+    }
 
+    /deep/ .is-process {
+
+      .el-step__icon {
+        background: #67C23A;
+        color: #FFFFFF;
+        border: 0;
+
+      }
+
+    }
+
+    /deep/ .el-step__head {
+      margin-bottom: 20px;
+    }
+
+    /deep/ .el-step__title.is-process {
+      color: #67C23A;
+    }
+
+    /deep/ .el-form-item__label {
+      width: 25%;
+      text-align: left;
+    }
+
+    /deep/ .el-form-item__content {
+      display: inline-block;
+      width: 70%;
+      margin-left: 0;
     }
 
   }
 
-  /deep/ .el-step__head {
-    margin-bottom: 20px;
+  .btn-wr {
+    text-align: center;
   }
 
-  /deep/ .el-step__title.is-process {
-    color: #67C23A;
+  .reset-success {
+    text-align: center;
+    margin: auto;
   }
 
-  /deep/ .el-form-item__label {
-    width: 25%;
-    text-align: left;
+  .set-success {
+    margin-top: 20px;
+    font-weight: 600;
   }
 
-  /deep/ .el-form-item__content {
-    display: inline-block;
-    width: 70%;
-    margin-left: 0;
-  }
+  .login-btn {
+    width: 200px;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+    margin: 0 auto;
+    background: #67C23A;
+    font-weight: 600;
+    font-size: 16px;
+    color: #FFFFFF;
+    border-radius: 4px;
 
-}
-
-.btn-wr {
-  text-align: center;
-}
-
-.reset-success {
-  text-align: center;
-  margin: auto;
-}
-
-.set-success {
-  margin-top: 20px;
-  font-weight: 600;
-}
-
-.login-btn {
-  width: 200px;
-  height: 50px;
-  text-align: center;
-  line-height: 50px;
-  margin: 0 auto;
-  background: #67C23A;
-  font-weight: 600;
-  font-size: 16px;
-  color: #FFFFFF;
-  border-radius: 4px;
-
-  i {
-    right: -50px;
-    font-size: 27px;
-    position: relative;
-    vertical-align: middle;
+    i {
+      right: -50px;
+      font-size: 27px;
+      position: relative;
+      vertical-align: middle;
     }
   }
 }
