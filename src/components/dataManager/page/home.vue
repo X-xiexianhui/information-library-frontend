88b4e9bd403e7a15818b3397c96e55b86fcdcbaa
xiexianhui <xiexianhuicn@outlook.com>
2022-04-30 11:00:31 +0800

    "data manage"

--- src/components/dataManager/page/home.vue
+++ src/components/dataManager/page/home.vue
@@ -3,7 +3,14 @@
     <div :class="$style.box">
       <div :class="$style.logo"><img src="../../../assets/logo.png" :class="$style.logo" alt="信息库管理系统"></div>
       <div :class="$style.btn">
-        <user-avatar></user-avatar>
+        <div class="text-btn">
+          <el-button type="text" @click="goToBack">
+            <i class="el-icon-arrow-left"></i>进入管理后台
+          </el-button>
+        </div>
+       <div>
+         <user-avatar></user-avatar>
+       </div>
       </div>
     </div>
     <el-container>
@@ -23,10 +30,15 @@ import DataMenu from '../components/dataMenu'
 import userAvatar from '../../userManager/components/userAvatar'
 export default {
   name: 'home',
-  components: {DataMenu, userAvatar}
+  components: {DataMenu, userAvatar},
+  methods: {
+    goToBack () {
+      this.$router.push('/db')
+    }
+  }
 }
 </script>
-<style module>
+<style module lang="less">
 .box {
   top: 0;
   position: fixed;
@@ -47,5 +59,13 @@ export default {
 }
 .btn{
   margin-right: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 10%;
 }
+.text-btn{
+  margin-right: 10px;
+}
+
 </style>
