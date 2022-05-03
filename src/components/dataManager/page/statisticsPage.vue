c44afd833fea4645164145f70699e0296bcb0e12
xiexianhui <xiexianhuicn@outlook.com>
2022-05-03 16:48:16 +0800

    "statistics"

--- src/components/dataManager/page/statisticsPage.vue
+++ src/components/dataManager/page/statisticsPage.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <el-form></el-form>
-    <div style="width: 100%; height: 100%;" ref="chart"></div>
+  <div style="width: 100%;height: 100%">
+    <div style="width: 60%; height: 60%;margin: auto" ref="chart"></div>
   </div>
 </template>
 
@@ -12,11 +11,13 @@ export default {
   name: 'statisticsPage',
   data () {
     return {
+      chart: null,
       form: {
         form_id: Number,
         field: '',
         group_field: '',
         option: '',
+        pictureType: 'bar',
         onlyUser: Boolean
       }
     }
@@ -33,9 +34,20 @@ export default {
       this.chart = Echarts.init(this.$refs.chart)
       // 3.配置数据
       let option = {
+        title: {
+          text: 'ECharts 入门示例',
+          left: 'center'
+        },
+        tooltip: {},
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: ['销量']
+        },
         xAxis: { type: 'category', data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }, // X轴
         yAxis: { type: 'value' }, // Y轴
-        series: [{ data: [120, 200, 150, 80, 70, 110, 130], type: 'bar' }] // 配置项
+        // eslint-disable-next-line standard/object-curly-even-spacing
+        series: [{ data: [120, 200, 150, 80, 70, 110, 130], type: this.form.pictureType, name: '销量'}] // 配置项
       }
       // 4.传入数据
       this.chart.setOption(option)
