3613f5a1cb8fe583aeb49fa0d307676a80a40f57
xiexianhui <xiexianhuicn@outlook.com>
2022-05-03 17:14:09 +0800

    "statistics"

--- src/components/dataManager/page/statisticsPage.vue
+++ src/components/dataManager/page/statisticsPage.vue
@@ -128,25 +128,8 @@ export default {
       // 2.初始化
       this.chart = Echarts.init(this.$refs.chart)
       // 3.配置数据
-      // let option = {
-      //   title: {
-      //     text: 'ECharts 入门示例',
-      //     left: 'center'
-      //   },
-      //   tooltip: {},
-      //   legend: {
-      //     orient: 'vertical',
-      //     left: 'left',
-      //     data: ['销量']
-      //   },
-      //   xAxis: { type: 'category', data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] }, // X轴
-      //   yAxis: { type: 'value' }, // Y轴
-      //   // eslint-disable-next-line standard/object-curly-even-spacing
-      //   series: [{ data: [120, 200, 150, 80, 70, 110, 130], type: this.form.pictureType, name: '销量'}] // 配置项
-      // }
-      let option = this.option
       // 4.传入数据
-      this.chart.setOption(option)
+      this.chart.setOption(this.option)
     }
   }
 }
