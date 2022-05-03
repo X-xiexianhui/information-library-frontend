f03ad522399a23bc7510f184e79757d375420130
xiexianhui <xiexianhuicn@outlook.com>
2022-05-03 18:07:57 +0800

    "statistics"

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import VXETable from 'vxe-table'
 import 'vxe-table/lib/style.css'
 import './assets/css/backend.css'
 import VueParticles from 'vue-particles'
+import VueCookies from "vue-cookies"
+Vue.use(VueCookies)
 Vue.use(VueParticles)
 Vue.use(VXETable)
 Vue.prototype.$http = axios
