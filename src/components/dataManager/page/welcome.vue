47bd94d674a49ec86ce1dea55897dd1165cf7d9e
xiexianhui <xiexianhuicn@outlook.com>
2022-05-10 17:39:35 +0800

    "dump data"

--- src/components/dataManager/page/welcome.vue
+++ src/components/dataManager/page/welcome.vue
@@ -1,6 +1,8 @@
 <template>
   <div style="margin-top: 20px">
     <div class="ms-title">欢迎使用信息库管理系统</div>
+    <img src="../../../assets/logo.png" class="imag" alt="信息库管理系统"/>
+    <p style="margin:30px">本系统是一个具有可拓展性和可复用性的信息库管理系统，用户可以定制所需的内容</p>
   </div>
 </template>
 
@@ -36,5 +38,10 @@ export default {
   margin: auto;
   font-size: 30px;
   color: cadetblue;
+}
+.imag{
+  margin-top: 10%;
+  text-align:center;
+  vertical-align:middle
 }
 </style>
