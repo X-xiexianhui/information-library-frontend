3e2fcab9feadefc22ae6c34950db597dee250fc5
xiexianhui <xiexianhuicn@outlook.com>
2022-05-10 15:47:10 +0800

    "dump data"

--- src/api/interctor.js
+++ src/api/interctor.js
@@ -1,11 +1,13 @@
 import router from '../router'
-import {MessageBox} from 'element-ui'
+import {MessageBox, Message} from 'element-ui'
 
 export function interceptor (res) {
   if (res.code === 401 || res.code === 402) {
+    // eslint-disable-next-line no-unused-expressions
+    Message.error('未登录，请先登录')
     return router.push('/')
   } else {
-    MessageBox.alert('未登录，请先登录', '警告', {
+    MessageBox.alert(res.msg, '警告', {
       confirmButtonText: '确定',
       callback: () => {
       }
